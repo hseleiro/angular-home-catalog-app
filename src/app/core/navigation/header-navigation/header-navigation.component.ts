f206e23db17811bb1fd6aea8b8eb38c3e90a9f2f
Hugo Seleiro <hseleiro@gmail.com>
2020-08-15 18:28:32 +0100

    navigation-header

---
+++ src/app/core/navigation/header-navigation/header-navigation.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NavigationOptions} from "../../models/navigation/navigation-options";
+
+@Component({
+  selector: 'app-header-navigation',
+  templateUrl: './header-navigation.component.html',
+  styleUrls: ['./header-navigation.component.scss']
+})
+export class HeaderNavigationComponent implements OnInit {
+
+  @Input() options: NavigationOptions[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
